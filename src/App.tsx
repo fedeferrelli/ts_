eb7ddaafb2af58d0323727c0acecb8c166c5dee1
Fede <ferrelli.mazza@gmail.com>
2022-10-25 19:47:16 -0300

    Add interface to define types in data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const MOCKED_DATA = [
+  {name: 'fede', lastName: 'ferrelli', age: 48, job: 'CFO', active: true}, 
+  {name: 'libera', lastName: 'ferrelli fuster', age: 4, job: 'CEO', active: false},
+  {name: 'pau', lastName: 'fuster', age: 35, job: 'CTO', active: true}
+]
+
+interface Staff {
+  name: string, 
+  lastName: string, 
+  age: number, 
+  job: string, 
+  active: boolean
+}
+
+
 
 function App() {
-const [number, setNumber] = useState(5)  
+const [staff, setStaff] = useState<Array<Staff>>([])
 
-const handleOnClick = () =>{
-  setNumber(number+1)
-}
+useEffect(() => {
+  setStaff(MOCKED_DATA)
+}, [])
+
 
   return (
     <div>
-     <h1>{number}</h1>
-     <button onClick={handleOnClick}> Click Me</button>
+      {staff.map(person=>{
+        return(
+          <div key={person.name}>
+            <img src={`https://i.pravatar.cc/150?img=${person.lastName}`}/>
+            <h1>{person.job}</h1>
+            <h2>{person.name} {person.lastName}</h2>
+            {person.active && <h2>Activo</h2>}
+          </div>
+        )
+        })}
     </div>
   )
 }
