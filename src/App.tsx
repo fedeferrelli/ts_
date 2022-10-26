f83b87ba20144c8fa1f5b196a12a2b090fc75583
Fede <ferrelli.mazza@gmail.com>
2022-10-25 21:18:27 -0300

    Install Tailwindcss and give some style

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,15 @@ useEffect(() => {
 
 
   return (
-    <div>
+    <div className="flex flex-col sm:flex-row justify-evenly pt-8 gap-4">
       {staff.map(person=>{
         return(
-          <div key={person.name}>
-            <img src={`https://i.pravatar.cc/150?img=${person.lastName}`}/>
+          <div key={person.name} className="w-11/12 p-2 flex flex-col border border-gray-200 rounded-md shadow-lg m-auto justify-center items-center sm:min-w-[250px] hover:bg-gray-100/25 hover:shadow-xl duration-300 ease-in-out relative">
+            <img className=" rounded-full w-[100px]" src={`https://i.pravatar.cc/150?img=${person.lastName}`}/>
             <h1>{person.job}</h1>
             <h2>{person.name} {person.lastName}</h2>
-            {person.active && <h2>Activo</h2>}
+            <h2>{person.age}</h2>
+            {person.active && <span className="absolute w-6 h-6 rounded-full bg-green-600/25 -top-3 -right-3 animate-ping"></span>}
           </div>
         )
         })}
