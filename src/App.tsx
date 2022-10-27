ca7545f749abe926c3937c6883b959417274d3ba
Fede <ferrelli.mazza@gmail.com>
2022-10-26 22:40:50 -0300

    Add new exercise

--- src/App.tsx
+++ src/App.tsx
@@ -52,8 +52,10 @@ const [movieData, setMovieData] = useState<Movies>(INITIAL_DATA);
 
       {movieData.results.map(movie =>{
         return(
+          <div key={movie.id}>
 
 <RenderMovie movieToShow={movie} />
+</div>
           /* <div key={movie.id} className="w-10/12 py-2 px-3 m-auto sm:w-[300px] border bg-slate-800/20 border-slate-700 rounded-[6px] shadow-lg mb-8 relative overflow-hidden hover:shadow-black/50 ease-in-out duration-300" onClick={()=>handleOnClickToShowOverview()}>
  <img className=" rounded-full w-full h-[350px]" src='https://image.tmdb.org/t/p/w500${movie.poster_path}' alt={`imagen para ${movie.title}`}/>
 <div className="w-full h-[350px] bg-slate-700/10 text-center text-slate-600">IMAGE</div>
